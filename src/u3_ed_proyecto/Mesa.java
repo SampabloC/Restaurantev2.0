6310828ad7b3c5df6647775258a5c86ad43f5ff2
DOC <haildoc69@gmail.com>
2018-11-16 08:52:01 -0600

    gg izi

--- src/u3_ed_proyecto/Mesa.java
+++ src/u3_ed_proyecto/Mesa.java
@@ -1,17 +1,16 @@
 package u3_ed_proyecto;
 
+import static u3_ed_proyecto.U3_ED_PROYECTO.leer;
+
 /**
  *
  * @author Javier Ismael Sampablo Cuevas
  */
 public class Mesa {
 
-    mesa sigue;
-    mesa ant;
-    mesa inicio;
-    mesa fin;
-
+    Boolean bol = true;
     class mesa {
+
         mesa sigue;
         mesa atras;
         String mesero;
@@ -21,10 +20,17 @@ public class Mesa {
         mesa platos;
     }
 
+    mesa sigue;
+    mesa ant;
+    mesa inicio;
+    mesa fin;
+
+   
+
     void Datos() {
-        
+
     }
-    
+
     void Insertar(mesa nuevo) {
         mesa aux;
         if (inicio == null) {
@@ -38,4 +44,39 @@ public class Mesa {
             }
         }
     }
+
+    void Registro() {
+        int nummes = 0;
+        mesa nuevo = new mesa();
+        System.out.println("Teclea el número de mesa: ");
+        do {
+            try {
+                nummes = leer.nextInt();
+                bol = true;
+                leer.nextLine();
+            } catch (java.util.InputMismatchException e) {
+                leer.nextLine();
+                System.out.println("No es un dato entero");
+                bol = false;
+            }
+        } while (bol == false);
+        nuevo.mesa=nummes;
+        System.out.print("Teclea el nombre del mesero: ");
+        nuevo.mesero= leer.nextLine();
+        System.out.print("Teclea el Nombre del Cliente: ");
+        nuevo.nombre=leer.nextLine();
+    }
+        void Mostrar() {
+        mesa aux = inicio;
+        if (aux == null) {
+            System.out.println("Mesas vacias");
+        } else {
+            System.out.println("Número de mesa\tMesero\tNombre");
+            while (aux != null) {
+                System.out.println(aux.mesa + " " + aux.mesero + " " + aux.nombre);
+                aux = aux.sigue;
+            }
+        }
+    }
+
 }
